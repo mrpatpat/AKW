1be604c118060d819bdc3ba4c59f99e5f762c8e2
mrpatpat <Adrian@Adrian-PC>
2015-01-14 20:27:56 +0100

    adding Javadoc

--- src/WaterCycle.java
+++ src/WaterCycle.java
@@ -1,41 +1,84 @@
+/**
+ * Datenstruktur für einen Wasserkreislauf.
+ * 
+ * @author Adrian, Felix
+ *
+ */
 public class WaterCycle {
-	
+
+	/**
+	 * Jedes Element ist ein Wärmespeicher.
+	 */
 	private volatile HeatStorage[] cycle;
+
+	/**
+	 * Zeiger auf das erste Element.
+	 */
 	private volatile int first;
 
+	/**
+	 * Größe des Kreislaufes.
+	 */
 	private final int size;
 
-	public WaterCycle(int startTemp, int size){
-		
+	/**
+	 * Konstruktor für den Kreislauf.
+	 * 
+	 * @param startTemp
+	 *            Starttemperatur aller Elemente
+	 * @param size
+	 *            Größe des Kreislaufes
+	 */
+	public WaterCycle(int startTemp, int size) {
+
 		this.size = size;
-	
+
 		cycle = new HeatStorage[size];
-		
-		for(int i=0;i<size;i++){
+
+		for (int i = 0; i < size; i++) {
 			cycle[i] = new Water(startTemp);
 		}
-		
+
 		first = 0;
-		
+
 	}
-	
-	public synchronized void rotate(){
+
+	/**
+	 * Rotiert alle Wasserelemente um 1.
+	 */
+	public synchronized void rotate() {
 		first++;
-		if(first>=size){
+		if (first >= size) {
 			first = 0;
 		}
 	}
-	
-	public synchronized HeatStorage get(int i){
-		return cycle[(i+first)%size];
-	}
-	
-	private class Water extends HeatStorage{
 
+	/**
+	 * Gibt das i. Element zurück.
+	 * 
+	 * @param i
+	 *            Index
+	 * @return Element an i
+	 */
+	public synchronized HeatStorage get(int i) {
+		return cycle[(i + first) % size];
+	}
+
+	/**
+	 * Innere Klasse für ein Wasserelement. Nur der Kreislauf benutzt es.
+	 * @author Adrian, Felix
+	 *
+	 */
+	private class Water extends HeatStorage {
+
+		/**
+		 * Konstruktor.
+		 * @param temp Starttemperatur
+		 */
 		public Water(int temp) {
 			super(temp);
 		}
-		
+
 	}
-	
+
 }
